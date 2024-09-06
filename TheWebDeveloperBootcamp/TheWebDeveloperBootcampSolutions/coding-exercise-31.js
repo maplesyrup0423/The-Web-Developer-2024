d7b88bb8eb1b770f3f4994a1b037283b4060b6b7
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-09-06 22:04:17 +0900

    코딩연습 31~34

---
+++ TheWebDeveloperBootcamp/TheWebDeveloperBootcampSolutions/coding-exercise-31.js
@@ -0,0 +1,4 @@
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+for (let index = 0; index < 6; index++) {
+  console.log("Da ba dee da ba daa");
+}
---
+++ TheWebDeveloperBootcamp/TheWebDeveloperBootcampSolutions/coding-exercise-34.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for (const element of numbers) {
+  console.log(element * element);
+}
